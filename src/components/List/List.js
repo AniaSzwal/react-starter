5b23e78eb2a65bde428624494c691bafb7787c71
Anna Szwal <aniaszwal@gmail.com>
2021-04-26 00:00:07 +0200

    list's and column's refactoring

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
-import Column from '../Column/Column';
+import Column from '../Column/ColumnContainer';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator';
+//import Creator from '../Creator/Creator';
 
 
 class List extends React.Component {
-    state = {
-      columns: this.props.columns || [],
-    }
 
     static propTypes = {
       title: PropTypes.node.isRequired,
@@ -24,37 +21,22 @@ class List extends React.Component {
       description: settings.defaultListDescription,
     }
 
-    addColumn(title){
-      this.setState(state => (
-        {
-          columns: [
-            ...state.columns,
-            {
-              key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-              title,
-              icon: 'list-alt',
-              cards: [],
-            },
-          ],
-        }
-      ));
-    }
 
     render() {
+        const {title, image, description, columns} = this.props;
+
       return (
         <section className={styles.component}>
-          <Hero titleText={this.props.title} imgSrcHero={this.props.image} />
-          <div className={styles.description}>
-            {ReactHtmlParser(this.props.description)}
-          </div>
-          <div className={styles.columns}>
-            {this.state.columns.map(({key, ...columnProps}) => (
-              <Column key={key} {...columnProps} />
+            <Hero titleText={title} image={image} />
+            <div className={styles.description}>
+                {ReactHtmlParser(description)}
+            </div>
+            {columns.map(columnData => (
+                <Column key={columnData.id} {...columnData} />
             ))}
-          </div>
-          <div className={styles.creator}>
+            {/* <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-          </div>
+          </div>*/}
         </section>
       );
     }
