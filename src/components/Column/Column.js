78269941b92d31855a54095bc25de4b208721743
Anna Szwal <aniaszwal@gmail.com>
2021-04-28 20:15:09 +0200

    changing state by props

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
+import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
 
@@ -11,10 +12,11 @@ class Column extends React.Component {
       title: PropTypes.node.isRequired,
       icon: PropTypes.node,
       cards: PropTypes.node,
+        addCard: PropTypes.func
     }
 
     render() {
-      const{title, icon, cards}=this.props;
+      const{title, icon, cards, addCard}=this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>{this.props.title}
@@ -23,9 +25,9 @@ class Column extends React.Component {
           {cards.map(cardData => (
             <Card key={cardData.id} {...cardData} />
           ))}
-          {/*<div className={styles.creator}>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-          </div>*/}
+          <div className={styles.creator}>
+            <Creator text={settings.cardCreatorText} action={addCard}/>
+          </div>
         </section>
       );
     }
