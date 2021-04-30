ca9335ebdea50f812b13e9e061a89c1e6c3919fc
Anna Szwal <aniaszwal@gmail.com>
2021-04-30 17:16:09 +0200

    ter doesn't work ;c

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore';
 import columnsReducer from './ColumnRedux';
 import cardsReducer  from './CardsRedux';
+import searchStringReducer from './searchStringRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -9,12 +10,17 @@ const initialState = {
   lists: initialStoreData.lists,
   columns: initialStoreData.columns,
   cards: initialStoreData.cards,
+  searchString: 'ter',
+
 };
 
 // define reducers
 const reducers = {
   columns: columnsReducer,
   cards: cardsReducer,
+  searchString: searchStringReducer,
+
+
 };
 
 // add blank reducers for initial state properties without reducers
