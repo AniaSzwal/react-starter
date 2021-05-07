01a0a65373b3c7fe4971cf83ae39b1cb772fa449
Anna Szwal <aniaszwal@gmail.com>
2021-05-07 13:42:28 +0200

    Link chore

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from  'react';
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import styles from './Header.scss.css';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
@@ -11,14 +11,14 @@ class Header extends React.Component {
       <header className={styles.component}>
         <Container>
           <div className={styles.wrapper}>
-            <Link className={styles.logo}>
+            <div className={styles.logo}>
               <Icon name={settings.header.icon} />
               <nav>
                 <NavLink exact to='/' activeClassName='active'>Home</NavLink>
                 <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
                 <NavLink exact to='FAQ' activeClassName='active'>FAQ</NavLink>
               </nav>
-            </Link>
+            </div>git
           </div>
         </Container>
       </header>
